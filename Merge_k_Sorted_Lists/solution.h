c50ea4c8aeaa7d6c4c7584e817a87e43dc1a1a1e
Yi Pan <Y.Pan@bham.ac.uk>
2019-07-10 19:56:37 +0100

    Update leetcode

--- Merge_k_Sorted_Lists/solution.h
+++ Merge_k_Sorted_Lists/solution.h
@@ -1,48 +1,56 @@
 #include <cstddef>
 
 #include <iostream>
+#include <iterator>
 #include <vector>
 
 struct ListNode {
-    int val;
-    ListNode *next;
-    ListNode(int x) : val(x), next(NULL) {}
+  int val;
+  ListNode *next;
+ListNode(int x) : val(x), next(NULL) {}
 };
 
 class Solution {
-public:
-  ListNode* mergeKLists(std::vector<ListNode *>& lists) {
-      ListNode *res = new ListNode(0);
-      ListNode *pl = res;
+ public:
+  ListNode* mergeTwoLists(ListNode *l1, ListNode *l2){
+    ListNode *res = new ListNode(0);
+    ListNode *pl = res;
 
-      bool flag = false;
-      for(auto &elem : lists) { if(elem) flag = true; }
-
-      while (flag) {
-	int min_val, min_idx, idx;
-	auto tmp = lists.begin();
-	idx = 0;
-	while(!(*tmp)) { ++tmp; ++idx; }
-	min_val = lists[idx]->val;
-	min_idx = idx;
-
-	idx = 0;
-	for (auto iter = lists.begin(); iter != lists.end(); ++iter, ++idx) {
-	  if (*iter && (*iter)->val < min_val) {
-	    min_val = (*iter)->val;
-	    min_idx = idx;
-	  }
-	}
-
-	lists[min_idx] = lists[min_idx]->next;
-
-	pl->next = new ListNode(min_val);
-	pl = pl->next;
-
-	flag = false;
-	for(auto &elem : lists) { if(elem) flag = true; }
+    while (l1 || l2) {
+      int val;
+      if (l1 && l2) {
+        if (l1->val <= l2->val) {
+          val = l1->val;
+          l1 = l1->next;
+        } else {
+          val = l2->val;
+          l2 = l2->next;
+        }
+      } else if (l1) {
+        val = l1->val;
+        l1 = l1->next;
+      } else {
+        val = l2->val;
+        l2 = l2->next;
       }
-
-      return res->next;
+      pl->next = new ListNode(val);
+      pl = pl->next;
     }
+
+    return res->next;
+  }
+
+  using iterator = std::vector<ListNode *>::iterator;
+  ListNode* mergeKLists(iterator iter1, iterator iter2) {
+    int distance = std::distance(iter1, iter2);
+    if (distance == 0) return NULL;
+    else if (distance == 1) return *iter1;
+    else if (distance == 2) return mergeTwoLists(*iter1, *std::next(iter1));
+    else return mergeTwoLists(mergeKLists(iter1, iter1 + distance/2), mergeKLists(iter1 + distance/2, iter2));
+  }
+
+
+  ListNode* mergeKLists(std::vector<ListNode *>& lists) {
+    return mergeKLists(lists.begin(), lists.end());
+  }
 };
