c4e610f647f972a1f8fdcc98f9463dfacbf00010
Yi Pan <ypan1988@gmail.com>
2019-07-17 10:24:08 +0100

    update leetcode

---
+++ Merge_Intervals/main.cc
@@ -0,0 +1,25 @@
+#include <iostream>
+#include <vector>
+#include "solution.h"
+
+int main()
+{
+  Solution s;
+  std::vector<std::vector<int>> vvec1 = {{1,3}, {2,6}, {8,10}, {15,18}};
+  std::vector<std::vector<int>> vvec2 = {{1,4}, {4,5}};
+
+  std::vector<std::vector<int>> res1 = s.merge(vvec1);
+  std::vector<std::vector<int>> res2 = s.merge(vvec2);
+
+  for(const auto & elem : res1) {
+    std::cout << " [" << elem[0] << "," << elem[1] << "] ";
+  }
+  std::cout << std::endl;
+
+  for(const auto & elem : res2) {
+    std::cout << " [" << elem[0] << "," << elem[1] << "] ";
+  }
+  std::cout << std::endl;
+
+  return 0;
+}
---
+++ Merge_Intervals/solution.h
@@ -0,0 +1,25 @@
+#include <algorithm>
+#include <vector>
+using std::vector;
+using std::sort;
+
+class Solution {
+public:
+  vector<vector<int>> merge(vector<vector<int>>& intervals) {
+    sort(intervals.begin(), intervals.end(),
+	 [](const vector<int> &interval1, const vector<int> &interval2){
+	   return interval1[0] < interval2[0];
+	 });
+
+    vector<vector<int>> res;
+    for(const auto &interval : intervals) {
+      if (!res.empty() && interval[0] <= res.back().at(1)) {
+	if (interval[1] > res.back().at(1))
+	  res.back().at(1) = interval[1];
+      } else {
+	res.push_back(interval);
+      }
+    }
+    return res;
+  }
+};
