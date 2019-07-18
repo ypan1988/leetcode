d92110b1e614152d643e5370b58e17f84bfbdaec
Yi Pan <ypan1988@gmail.com>
2019-07-18 15:17:34 +0100

    update leetcode

---
+++ Remove_Duplicates_from_Sorted_List_II/main.cc
@@ -0,0 +1,38 @@
+#include <initializer_list>
+#include <iostream>
+#include "solution.h"
+
+using namespace std;
+
+ListNode* create_linkedlists(initializer_list<int> lst) {
+  ListNode *res = new ListNode(0);
+  ListNode *tail = res;
+  for (auto iter = lst.begin(); iter != lst.end(); ++iter) {
+    tail->next = new ListNode(*iter);
+    tail = tail->next;
+  }
+
+  return res->next;
+}
+
+int main()
+{
+  Solution s;
+  ListNode *l1 = create_linkedlists({1,2,3,3,4,4,5});
+  ListNode *l2 = create_linkedlists({1,1,1,2,3});
+
+  ListNode *res1 = s.deleteDuplicates(l1);
+  ListNode *res2 = s.deleteDuplicates(l2);
+
+  for (auto cur = res1; cur != NULL; cur = cur->next) {
+    cout << cur->val << "->";
+  }
+  cout << "NULL" << endl;
+
+  for (auto cur = res2; cur != NULL; cur = cur->next) {
+    cout << cur->val << "->";
+  }
+  cout << "NULL" << endl;
+
+  return 0;
+}
---
+++ Remove_Duplicates_from_Sorted_List_II/solution.h
@@ -0,0 +1,36 @@
+#include <cstddef>
+
+struct ListNode{
+  int val;
+  ListNode *next;
+  ListNode(int x): val(x), next(NULL) {}
+};
+
+class Solution {
+public:
+  ListNode* deleteDuplicates(ListNode* head) {
+    ListNode *res = new ListNode(0);
+    ListNode *tail = res;
+
+    while (head) {
+      int curr_val = head->val;
+      head = head->next;
+
+      bool flag_dup = false;
+      while (head && head->val == curr_val) {
+	head = head->next;
+	flag_dup = true;
+      }
+
+      if (!flag_dup) {
+	tail->next = new ListNode(curr_val);
+	tail = tail->next;
+      }
+    }
+
+    tail->next = NULL;
+    tail = tail->next;
+
+    return res->next;
+  }
+};
