c4b1741847a2751238da419f6d0f0a7f582052e1
Yi Pan <Y.Pan@bham.ac.uk>
2019-12-03 18:34:25 +0000

    Update 3Sum

--- 3Sum/Solution.h
+++ 3Sum/Solution.h
@@ -1,6 +1,8 @@
 #include <algorithm>
 #include <vector>
 
+using namespace std;
+
 class Solution {
  public:
   int binary_search(int key, const vector<int>& ivec, int lo, int hi) {
@@ -20,16 +22,12 @@ class Solution {
 
     vector<vector<int>> ans;
     for (int i = 0; i < nums.size(); ++i) {
+      if (i > 0 && nums[i] == nums[i - 1]) continue;
       for (int j = i + 1; j < nums.size(); ++j) {
+        if (j > i + 1 && nums[j] == nums[j - 1]) continue;
         int k =
             binary_search(-(nums[i] + nums[j]), nums, j + 1, nums.size() - 1);
-        if (k == -1) continue;
-        if (!ans.empty()) {
-          vector<int> back = ans.back();
-          if (back[0] == nums[i] && back[1] == nums[j] && back[2] == nums[k])
-            continue;
-        }
-        ans.push_back({nums[i], nums[j], nums[k]});
+        if (k != -1) ans.push_back({nums[i], nums[j], nums[k]});
       }
     }
     return ans;
