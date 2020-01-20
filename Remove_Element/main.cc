d9c82eb7656a99f5b32e1b35c981dcaff0de07e3
Yi Pan <ypan1988@gmail.com>
2020-01-20 12:05:31 +0000

    remove element

---
+++ Remove_Element/main.cc
@@ -0,0 +1,24 @@
+#include <iostream>
+#include <vector>
+
+using namespace std;
+
+#include "solution.h"
+
+int main() {
+  vector<int> ivec1 = {3, 2, 2, 3};
+  int ival1 = 3;
+
+  vector<int> ivec2 = {0, 1, 2, 2, 3, 0, 4, 2};
+  int ival2 = 2;
+
+  vector<int> ivec3 = {1};
+
+  Solution sol;
+
+  cout << sol.removeElement(ivec1, ival1) << endl;
+  cout << sol.removeElement(ivec2, ival2) << endl;
+  cout << sol.removeElement(ivec3, 1) << endl;
+
+  return 0;
+}
---
+++ Remove_Element/solution.h
@@ -0,0 +1,26 @@
+#include <vector>
+using namespace std;
+
+class Solution {
+ public:
+  int removeElement(vector<int>& nums, int val) {
+    if (nums.empty()) return 0;
+
+    int i = 0;
+    int j = nums.size() - 1;
+    while (true) {
+      while (i <= nums.size() - 1 && nums[i] != val) {
+        ++i;
+      }
+      while (j >= 0 && nums[j] == val) {
+        --j;
+      }
+      if (i >= j) break;
+      swap(nums[i], nums[j]);
+    }
+
+    nums.erase(nums.begin() + i, nums.end());
+
+    return nums.size();
+  }
+};
