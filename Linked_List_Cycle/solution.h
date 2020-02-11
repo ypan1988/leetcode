d4ec856bf2cb2b7b9a197d43cd3e9d43122f56c1
Yi Pan <ypan1988@gmail.com>
2020-02-11 20:19:56 +0000

    using hash set & two pointers

--- Linked_List_Cycle/solution.h
+++ Linked_List_Cycle/solution.h
@@ -1,21 +1,23 @@
 #include <cstddef>
+#include <unordered_set>
+using namespace std;
 
 struct ListNode {
   int val;
   ListNode *next;
   ListNode(int x) : val(x), next(NULL) {}
 };
+
 class Solution {
  public:
   bool hasCycle(ListNode *head) {
-    if (!head) return false;
-
-    ListNode *slow = head;
-    ListNode *fast = head;
-    while (fast->next && fast->next->next) {
-      slow = slow->next;
-      fast = fast->next->next;
-      if (slow == fast) return true;
+    unordered_set<ListNode *> s;
+    while (head) {
+      if (s.count(head))
+        return true;
+      else
+        s.insert(head);
+      head = head->next;
     }
     return false;
   }
